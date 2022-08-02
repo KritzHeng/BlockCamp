97d2b85589c3b157d5160602584146cb1e4b706f
panda3141592 <panda3141592@gmail.com>
2022-08-02 21:58:15 +0700

    daoProject layout

--- pages/daoProject.tsx
+++ pages/daoProject.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import {Card, Row, Col} from 'antd';
+import {Card, Row, Col, Table} from 'antd';
 import 'antd/dist/antd.css';
 import { UserOutlined } from '@ant-design/icons';
 
@@ -22,10 +22,42 @@ const Transactions = () => {
     const [name, setName] = useState("");
     const [description, setDescription] = useState("");
 
+    const columns = [
+        {
+            title: 'Address',
+            dataIndex: 'address',
+            key: 'address',
+        },
+        {
+          title: 'Type',
+          dataIndex: 'type',
+          key: 'type',
+        },
+        {
+          title: 'Value(USD)',
+          dataIndex: 'value',
+          key: 'value',
+        },
+        {
+          title: 'Chain',
+          dataIndex: 'chain',
+          key: 'chain',
+        },
+      ];
+      const data = [
+        {
+          key: '1',
+          address: 'test',
+          type: 'test',
+          value: 'test',
+          chain: 'test',
+        },
+      ];
+
     useEffect(() => {
-        setName("Uniswap")
-        setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
-      }, []);
+    setName("Uniswap")
+    setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+    }, []);
 
     return (
         <div style={{padding: "50px"}}>      
@@ -33,24 +65,15 @@ const Transactions = () => {
             <p>{description}</p>
             <h2>Revenue Model Of</h2>
             <Row>
-                <Col span={12}>
-                    <Card
-                        style={{
-                        width: 300,
-                        }}
-                    >
-                        <p>Card content</p>
-                        <p>Card content</p>
-                        <p>Card content</p>
-                    </Card>
+                <Col span={12} style={{paddingRight: "20px"}}>
+                    <Card>Placeholder</Card>
                 </Col>
-                <Col span={12}>3</Col>
+                <Col span={12}>
+                    <Card>Graph</Card>
+                </Col>
             </Row>
             <h2>Treasury Addresses</h2>
-            <Row>
-                <Col span={12}>4</Col>
-                <Col span={12}>5</Col>
-            </Row>
+            <Table columns={columns} dataSource={data}/>
         </div>
     );
 };
