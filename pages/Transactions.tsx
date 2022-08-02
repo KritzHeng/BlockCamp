405a4f68de2995638875ce4aef118b8e95682092
panda3141592 <panda3141592@gmail.com>
2022-08-02 14:23:02 +0700

    Basic ant UI

--- pages/Transactions.tsx
+++ pages/Transactions.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {Modal, Button, Row, Col, Avatar} from 'antd';
+import {Modal, Button, Row, Col, Avatar, Table, Tag, Space} from 'antd';
 import 'antd/dist/antd.css';
 import { UserOutlined } from '@ant-design/icons';
 
@@ -172,6 +172,80 @@ const Transactions = () => {
     },
   ]);
 
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    },
+    {
+      title: 'Tags',
+      key: 'tags',
+      dataIndex: 'tags',
+      render: (_, { tags }) => (
+        <>
+          {tags.map((tag) => {
+            let color = tag.length > 5 ? 'geekblue' : 'green';
+  
+            if (tag === 'loser') {
+              color = 'volcano';
+            }
+  
+            return (
+              <Tag color={color} key={tag}>
+                {tag.toUpperCase()}
+              </Tag>
+            );
+          })}
+        </>
+      ),
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (_, record) => (
+        <Space size="middle">
+          <a>Invite {record.name}</a>
+          <a>Delete</a>
+        </Space>
+      ),
+    },
+  ];
+  const data = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+      tags: ['nice', 'developer'],
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+      tags: ['loser'],
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+      tags: ['cool', 'teacher'],
+    },
+  ];
+
   const showModal = () => {
     setIsModalVisible(true);
   };
@@ -219,6 +293,7 @@ const Transactions = () => {
           </div>
         );
       })}
+      <Table columns={columns} dataSource={data} />
     </div>
   );
 };
