2b5c9ca2e066125d982f8c780d3fcd4557288968
Aom Wara <waranat.cpe@gmail.com>
2022-08-03 20:42:21 +0700

    refactor: chart

--- pages/ChartD3.tsx
+++ pages/ChartD3.tsx
@@ -1,70 +1,52 @@
-// import React, { useEffect, useState } from 'react';
-// import * as d3 from "d3-hierarchy";
-// // import {howto} from "@d3/example-components"
-// // import {Tree} from "@d3/tree"
+import Tree from 'react-d3-tree';
 
-// export default () => {
-//   const data = {
-//     "name": "Eve",
-//     "children": [
-//       {
-//         "name": "Cain"
-//       },
-//       {
-//         "name": "Seth",
-//         "children": [
-//           {
-//             "name": "Enos"
-//           },a
-//           {
-//             "name": "Noam"
-//           }
-//         ]
-//       },
-//       {
-//         "name": "Abel"
-//       },
-//       {
-//         "name": "Awan",
-//         "children": [
-//           {
-//             "name": "Enoch"
-//           }
-//         ]
-//       },
-//       {
-//         "name": "Azura"
-//       }
-//     ]
-//   }
-
-//   const [isReady, setIsReady] = useState(false)
-
-//   useEffect(() => {
-//     setIsReady(true)
-//   }, [])
-//   // function children(d) {
-//   //   return d.children;
-//   // }
-//   var treemap = d3.treemap()
-//     .size([width, height])
-//     .padding(2);
-
-// var nodes = treemap(root
-//     .sum(function(d) { return d.value; })
-//     .sort(function(a, b) { return b.height - a.height || b.value - a.value; }))
-//   .descendants();
-//   function children(d) {
-//     return Array.isArray(d) ? d[1] : null;
-//   }
-//   return (
-//     <div>
-      
-//       {
-//         isReady && (
-          
-//         )
-//       }
-//     </div>
-//   );
-// };
+const TreeChart = () => {
+  const orgChart = {
+    name: 'CEO',
+    children: [
+      {
+        name: 'Manager',
+        attributes: {
+          department: 'Production',
+        },
+        children: [
+          {
+            name: 'Foreman',
+            attributes: {
+              department: 'Fabrication',
+            },
+            children: [
+              {
+                name: 'Worker',
+              },
+            ],
+          },
+          {
+            name: 'Foreman',
+            attributes: {
+              department: 'Assembly',
+            },
+            children: [
+              {
+                name: 'Worker',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  };
+  return (
+    <>
+      <div id="treeWrapper" style={{ width: '1000px', height: '500px' }}>
+        <Tree
+          data={orgChart}
+          rootNodeClassName="node__root"
+          branchNodeClassName="node__branch"
+          leafNodeClassName="node__leaf"
+        />
+      </div>
+    </>
+  );
+};
+export default TreeChart;
